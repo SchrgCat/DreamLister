e1c5c8c3109702827048250dd2ca7b7c72ae5cf6
Mikołaj Skawiński <mikolajskawinski@MacBook-Air-Mikoaj.local>
2017-07-24 00:40:34 +0200

    Connect views to code

---
+++ DreamLister/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  DreamLister
+//
+//  Created by Mikołaj Skawiński on 24.07.2017.
+//  Copyright © 2017 Mikołaj Skawiński. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
