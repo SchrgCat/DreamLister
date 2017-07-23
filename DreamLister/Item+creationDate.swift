80fa848e809b3fe12f168e8f39a113f7f621d32f
Mikołaj Skawiński <mikolajskawinski@MacBook-Air-Mikoaj.local>
2017-07-23 15:39:20 +0200

    design main view

---
+++ DreamLister/Item+creationDate.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  DreamLister
+//
+//  Created by Mikołaj Skawiński on 23.07.2017.
+//  Copyright © 2017 Mikołaj Skawiński. All rights reserved.
+//
+
+import Foundation
---
+++ DreamLister/UIView+materialDesign.swift
@@ -0,0 +1,14 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Mikołaj Skawiński on 23.07.2017.
+//  Copyright © 2017 Mikołaj Skawiński. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @
+
+}
